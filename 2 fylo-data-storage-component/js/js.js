081bff77642f13dee4670aef3d02bcce32f5d98e
Monica Tatiana <Monica Tatiana Cuervo Rios>
2021-08-23 19:15:07 -0500

    segundo ejercicio js

--- 2 fylo-data-storage-component/js/js.js
+++ 2 fylo-data-storage-component/js/js.js
@@ -1,29 +1,37 @@
-var parameters = {
-    target: '#myFunction',
-    data: [{
-      fn: 'sin(x)', 
-      color: 'red'
-   }       
-          ],
-    grid: true,
-    yAxis: {domain: [-1, 1]},
-    xAxis: {domain: [0, 2*Math.PI]}
-  };
-  
-  function plot(){
-    
-    var f = document.querySelector("#function").value;
-    var xMin = document.querySelector("#xMin").value;
-    var xMax = document.querySelector("#xMax").value;
-    var yMin = document.querySelector("#yMin").value;
-    var yMax = document.querySelector("#yMax").value;
-    var color = document.querySelector("#color").value;
-  
-    parameters.data[0].fn = f;
-    parameters.xAxis.domain = [xMin, xMax];
-    parameters.yAxis.domain = [yMin, yMax];
-    parameters.data[0].color = color;
-    
-    functionPlot(parameters);  
-  }
-  
+let creador =  function (dato,tipo,nombre){
+let crear = document.createElement(dato);
+
+crear.setAttribute(tipo, nombre);
+return crear;
+}
+
+let img = creador("img","class", "colorbajo");
+img.src="./images/bg-desktop.png";
+document.body.append(img);
+
+let container = creador ("div", "class", "container");
+let logocontainer = creador ("div", "class", "logo-container");
+let imagen = creador ("img", "src", "./images/logo.svg");
+let icon = creador ("div", "class", "icon-collection");
+
+
+
+
+imagen.append(icon);
+logocontainer.append(imagen);
+container.append(logocontainer);
+document.body.append(container);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
