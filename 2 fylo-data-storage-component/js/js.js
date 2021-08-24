849aedc06142265ca764660010f5462004cb54a0
Monica Tatiana <Monica Tatiana Cuervo Rios>
2021-08-24 13:13:58 -0500

    cambio

--- 2 fylo-data-storage-component/js/js.js
+++ 2 fylo-data-storage-component/js/js.js
@@ -6,21 +6,38 @@ return crear;
 }
 
 let img = creador("img","class", "colorbajo");
-img.src="./images/bg-desktop.png";
-document.body.append(img);
-
+img.src="/images/bg-desktop.png";
 let container = creador ("div", "class", "container");
 let logocontainer = creador ("div", "class", "logo-container");
+let aux = creador ("div","class","aux");
 let imagen = creador ("img", "src", "./images/logo.svg");
 let icon = creador ("div", "class", "icon-collection");
+let div = creador ("div", "class", "icon");
+let imgicon = creador("img", "src", "./images/icon-document.svg");
+let icon2 = creador("div", "class", "icon");
+let imgicon2 = creador("img", "src", "./images/icon-folder.svg");
+let icon3 = creador("div", "src", "icon");
+let imgicon3 = creador ("img", "src", "./images/icon-upload.svg");
 
 
 
-
-imagen.append(icon);
-logocontainer.append(imagen);
-container.append(logocontainer);
 document.body.append(container);
+container.append(logocontainer);
+logocontainer.append(aux);
+aux.append(imagen);
+document.body.append(img);
+aux.append(icon);
+aux.append(icon2);
+icon.append(div);
+div.append(imgicon);
+icon2.append(imgicon2);
+
+
+
+
+
+
+
 
 
 
