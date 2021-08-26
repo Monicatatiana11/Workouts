1b1f031ac1979843a5c357a35498a7439addd605
Monica Tatiana <Monica Tatiana Cuervo Rios>
2021-08-26 16:53:09 -0500

    ultimo cambio js

--- 6 intro-component-with-signup-form/js/Js.js
+++ 6 intro-component-with-signup-form/js/Js.js
@@ -23,12 +23,17 @@ let input3 = creador("input", "class", "input3");
 let input4 = creador("input", "class", "input4");
 let boton = creador("button", "class", "boton");
     boton.innerText="CLAMIN YOUR FREE TRIAL";
+let mensaje_error = creador("span", "class", "mensaje_error");
+    mensaje_error.innerText="Verifica todos los campos";
+
+
 
 
 input1.placeholder="Nombre";
 input2.placeholder="Last name";
 input3.placeholder="Email Addres";
 input4.placeholder="Password";
+
 
 
 document.body.append(main);
@@ -44,19 +49,43 @@ formulario.append(input2);
 formulario.append(input3);
 formulario.append(input4);
 formulario.append(boton);
-input1.append(letras);
-boton.append(Textver);
+formulario.append(mensaje_error);
 
 
 
+let lista_inputs = Array.from(document.querySelectorAll("input"));
+lista_inputs.map((input) => {
+    input.addEventListener("keydown", () => { 
+        switch (input.placeholder != "password") {
+            case true:
+                let validaciones_para_nombres = new RegExp("^([A-ZÁÉÍÓÚ]{1}[a-zñáéíóú]+[\s]*)+$");
+                let validacion_correo = new RegExp("[a-zA-Z0-9._-]+@[a-zA-Z]+\.+com");
 
-
-
-
-
-
-
-
-
-
-
+                if (input.value == "") {
+                    mensaje_error.style.display = "flex";
+                } else if (input.placeholder == "Email Addres") {
+                    if (!validaciones_correo.test(input.value)) {
+                        input.classList.add("error_validacion");
+                        mensaje_error.style.display = "none";
+                    }
+                }  else if (input.placeholder == "Nombre" || input.placeholder == "Apellido") {
+                    if (!validaciones_para_nombres.test(input.value)) {
+                        input.classList.add("error_validacion");
+                        mensaje_error.style.display = "none";
+                    }
+                }
+                break;
+            case false:
+         
+                if (input == ""){
+                    mensaje_error.style.display = "flex";
+                }   else {
+                    if (input.value.length < 8){
+                        input.classList.add("error_validacion");
+                        mensaje_error.style.display = "none";
+                    }
+                }
+                
+        }
+    })
+})
