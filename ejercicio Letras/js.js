bec85223b645e396a2056f1f7c54a71767a3cd62
Monica Tatiana <Monica Tatiana Cuervo Rios>
2021-09-02 14:20:44 -0500

    ejercicioHarold

--- ejercicio Letras/js.js
+++ ejercicio Letras/js.js
@@ -1,24 +1,27 @@
-const aTags = document.querySelectorAll("a");
-const highlight = document.createElement("span");
+const aTags = document.querySelectorAll("a");  //declarando  constante que es igual  a los elementos//
+const highlight = document.createElement("span"); // constante que esta creando elemento span//
+highlight.classList.add("highlight"); // es la constante que esta creando la class //
+document.body.appendChild(highlight); // higfligth aparecera en body //
 
-highlight.classList.add("highlight");
-document.body.appendChild(highlight);
-
-for (let i = 0; i <aTags.length; i++){
-
-aTags[i].addEventListener("mouseenter",function () {
-
-this.style.transform= scale (2)";
-
-
-
-const linkCoords=this.getBoundingClient Rect();
-
-const coards = {
-
+for (let i = 0; i < aTags.length; i++) {   //expresión evaluada al final//
+aTags[i].addEventListener("mouseenter", function () { // verifica si el mouse esta encima del elemento//
+const linkCoords = this.getBoundingClientRect(); //la variable linkcoords es igual a las coordenadas del elemento donde esta el mouse//
+const coords = { 
 width: linkCoords.width,
+height: linkCoords.height,
+top: linkCoords.top + window.scrollY,
+left: linkCoords.left + window.scrollX,
+};
+// :* :* :* :* :* :P :P si te quiero ahogar jajaja :P :P :* :* :* :* :* 
+highlight.style.width = `${coords.width}px`,
+highlight.style.height = `${coords.height}px`,
+highlight.style.transform = `translate(${coords.left}px, ${coords.top}px)`;
+this.style.transform = `scale(2)`;
+});
 
-height: linkCoords.height, top: linkCoords.top + window.scrolly,
+aTags[i].addEventListener("mouseleave", function(){
+this.style.transform = `scale(1)`;
+})
 
-left: linkCoords.left + window.scrollx,
 
+}
