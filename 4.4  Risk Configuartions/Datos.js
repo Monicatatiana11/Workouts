77c4d02a93efc326355cd706a53cb3b7991d2a02
Monica Tatiana <Monica Tatiana Cuervo Rios>
2021-10-01 17:27:54 -0500

    ejercicio4.4

---
+++ 4.4  Risk Configuartions/Datos.js
@@ -0,0 +1,30 @@
+var Datos = [
+{
+     Titulo: "IMPACTOS",
+     tipoBoton: "Nuevo",
+     columnas: ["codigo", "descripcion", "valor", "Acción"]
+},
+{
+    Titulo: "PROBABILIDADES",
+    tipoBoton: "Nueva",
+    columnas: ["codigo", "descripcion", "valor", "Acción"]
+
+},
+{
+    Titulo: "TIPOS DE RIESGO",
+    tipoBoton: "Nuevo Tipo",
+    columnas: ["codigo", "descripcion", "valor", "Acción"]
+},
+
+{
+    Titulo: "EVALUACIÓN DEL RIESGOS",
+    tipoBoton: "Nuevo",
+    columnas: ["codigo", "Descripcion", "valor minimo", "Valor máximo", "Acción"]
+}
+
+]
+export { Datos }
+
+
+
+
